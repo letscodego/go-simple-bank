b26bf9684a54797e35cd09d7231ce7de64d2d044
K1 <searchfordeveloping@gmail.com>
2023-04-01 14:43:15 -0700

    testing sh

--- start.sh
+++ start.sh
@@ -3,6 +3,7 @@
 set -e
 
 echo "run db migration"
+source /app/app.env
 /app/migrate -path /app/migration -database "mysql://root:U1YBwRVT0Piz1BHWhq2Q@tcp(simple-bank.c9l2qt4dd8vn.us-east-1.rds.amazonaws.com:3306)/simple_bank" -verbose up
 
 echo "start the app"
