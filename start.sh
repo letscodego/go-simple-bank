0d074c7cbb37a4952a7ece2051fbbf11f35920dc
K1 <searchfordeveloping@gmail.com>
2023-04-01 14:02:06 -0700

    hard coded

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 set -e
 
 echo "run db migration"
-/app/migrate -path /app/migration -database "$MIGRATE_SOURCE" -verbose up
+/app/migrate -path /app/migration -database "mysql://root:U1YBwRVT0Piz1BHWhq2Q@tcp(simple-bank.c9l2qt4dd8vn.us-east-1.rds.amazonaws.com:3306)/simple_bank" -verbose up
 
 echo "start the app"
 exec "$@"
