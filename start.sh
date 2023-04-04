c12af9f42930eefaf0150a8858de57d5ccd312f9
K1 <searchfordeveloping@gmail.com>
2023-04-03 17:32:59 -0700

    modify sh file

--- start.sh
+++ start.sh
@@ -1,10 +1,11 @@
-#!/bin/sh
+#!bin/bash
 
 set -e
 
 echo "run db migration"
-
-/app/migrate -path /app/migration -database "mysql://root:U1YBwRVT0Piz1BHWhq2Q@tcp(simple-bank.c9l2qt4dd8vn.us-east-1.rds.amazonaws.com:3306)/simple_bank" -verbose up
+source /app/app.env
+echo $MIGRATE_SOURCE
+/app/migrate -path /app/migration -database $MIGRATE_SOURCE -verbose up
 
 echo "start the app"
 exec "$@"
